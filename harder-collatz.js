4d0b714e9148e5a433f0b999c46bb8a0627756be
joshuapena <penacalata@gmail.com>
2014-05-15 12:08:34 -0700

    update

--- harder-collatz.js
+++ harder-collatz.js
@@ -3,7 +3,7 @@ console.log("Joshua's Collatz");
 function collatz(x) {
 	seq = [x];
 	while (x > 1) {
-		if (x % 2 == 0) {
+		if (x % 2 === 0) {
 			x = x / 2;
 		} else {
 			x = 3 * x + 1;
@@ -13,16 +13,18 @@ function collatz(x) {
 	return seq;
 }
 
+/*
 if (isNaN(process.argv[2])) {
 	console.log("Usage : collatz starting-number (default is 10)");
 	var start = 10;
 } else {
 	var start = parseInt(process.argv[2]);
 }
+*/
 
-//var start = isNaN(process.argv[2]) ? 10 : parseInt(process.argv[2]);
+var start = isNaN(process.argv[2]) ? 10 : parseInt(process.argv[2]);
 
 var seq = collatz(start);
-var length = seq.length;
+var size = seq.length;
 console.log(seq);
-console.log("collatz(%d) has %d items", start, length);
+console.log("collatz(%d) has %d items", start, size);
